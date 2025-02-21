0018dab4ece742b770101cea006f26665b474a0d
BerlianDutaKrisna <berliandutakrisna@gmail.com>
2025-02-21 14:38:22 +0700

    a

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -5,28 +5,32 @@ namespace App\Controllers;
 use App\Models\HpaModel;
 use App\Models\ProsesModel\PenerimaanModel;
 
-
 class Dashboard extends BaseController
 {
+    protected $hpaModel;
+    protected $penerimaanModel;
+    protected $session;
+
+    public function __construct()
+    {
+        // Menggunakan layanan CodeIgniter untuk manajemen model
+        $this->hpaModel = model(HpaModel::class);
+        $this->penerimaanModel = model(PenerimaanModel::class);
+        $this->session = session();
+    }
+
     public function index()
     {
-
-        $hpaModel = new HpaModel();
-        $penerimaanModel = new PenerimaanModel();
-        $hpaData = $hpaModel->getHpaWithRelations();
-        $countHpaProcessed = $hpaModel->countHpaProcessed();
-        $countPenerimaan = $penerimaanModel->countPenerimaan();
-        $chartData = $hpaModel->getHpaChartData();
-        $chartDataJson = json_encode($chartData, JSON_NUMERIC_CHECK);
-
+        // Mengambil data dari model
         $data = [
-            'hpaData' => $hpaData,
-            'countHpa' => $countHpaProcessed,
-            'countPenerimaan' => $countPenerimaan,
-            'id_user' => session()->get('id_user'),
-            'nama_user' => session()->get('nama_user'),
-            'chartData' => $chartDataJson // Data dalam format JSON
+            'hpaData' => $this->hpaModel->getHpaWithRelations(),
+            'countHpa' => $this->hpaModel->countHpaProcessed(),
+            'countPenerimaan' => $this->penerimaanModel->countPenerimaan(),
+            'id_user' => $this->session->get('id_user'),
+            'nama_user' => $this->session->get('nama_user'),
+            'chartData' => json_encode($this->hpaModel->getHpaChartData(), JSON_NUMERIC_CHECK)
         ];
+
         return view('dashboard/dashboard', $data);
     }
 }
