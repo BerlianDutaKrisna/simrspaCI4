7287de7b6e7625de30f71020460c4ff5e59f76b1
BerlianDutaKrisna <berliandutakrisna@gmail.com>
2025-03-26 09:02:40 +0700

    tampilan data

--- app/Models/PatientModel.php
+++ app/Models/PatientModel.php
@@ -49,30 +49,65 @@ class PatientModel extends Model
     public function getPatientWithRelations()
     {
         $query = "
-        SELECT patient.*, 
-            hpa.kode_hpa AS kode_pemeriksaan, hpa.tanggal_permintaan, 'HPA' AS jenis_pemeriksaan,
-            hpa.hasil_hpa AS hasil, hpa.status_hpa AS status, hpa.penerima_hpa AS penerima, hpa.tanggal_penerima
-        FROM patient
-        LEFT JOIN hpa ON hpa.id_pasien = patient.id_pasien
-        UNION ALL
-        SELECT patient.*, 
-            frs.kode_frs AS kode_pemeriksaan, frs.tanggal_permintaan, 'FRS' AS jenis_pemeriksaan,
-            frs.hasil_frs AS hasil, frs.status_frs AS status, frs.penerima_frs AS penerima, frs.tanggal_penerima
-        FROM patient
-        LEFT JOIN frs ON frs.id_pasien = patient.id_pasien
-        UNION ALL
-        SELECT patient.*, 
-            srs.kode_srs AS kode_pemeriksaan, srs.tanggal_permintaan, 'SRS' AS jenis_pemeriksaan,
-            srs.hasil_srs AS hasil, srs.status_srs AS status, srs.penerima_srs AS penerima, srs.tanggal_penerima
-        FROM patient
-        LEFT JOIN srs ON srs.id_pasien = patient.id_pasien
-        UNION ALL
-        SELECT patient.*, 
-            ihc.kode_ihc AS kode_pemeriksaan, ihc.tanggal_permintaan, 'IHC' AS jenis_pemeriksaan,
-            ihc.hasil_ihc AS hasil, ihc.status_ihc AS status, ihc.penerima_ihc AS penerima, ihc.tanggal_penerima
-        FROM patient
-        LEFT JOIN ihc ON ihc.id_pasien = patient.id_pasien
-        ORDER BY tanggal_permintaan DESC, kode_pemeriksaan ASC
+            SELECT p.id_pasien, 
+        p.norm_pasien, 
+        p.nama_pasien, 
+        p.alamat_pasien, 
+        p.tanggal_lahir_pasien, 
+        p.jenis_kelamin_pasien, 
+        hpa.unit_asal, hpa.dokter_pengirim, hpa.diagnosa_klinik, 
+        hpa.kode_hpa AS kode_pemeriksaan, hpa.tanggal_permintaan, 'HPA' AS jenis_pemeriksaan,
+        hpa.hasil_hpa AS hasil, hpa.status_hpa AS status, hpa.penerima_hpa AS penerima, hpa.tanggal_penerima
+    FROM patient p
+    INNER JOIN hpa ON hpa.id_pasien = p.id_pasien 
+    WHERE hpa.tanggal_permintaan BETWEEN CURDATE() - INTERVAL 14 DAY AND CURDATE()
+
+    UNION ALL
+
+    SELECT p.id_pasien, 
+        p.norm_pasien, 
+        p.nama_pasien, 
+        p.alamat_pasien, 
+        p.tanggal_lahir_pasien, 
+        p.jenis_kelamin_pasien, 
+        frs.unit_asal, frs.dokter_pengirim, frs.diagnosa_klinik, 
+        frs.kode_frs AS kode_pemeriksaan, frs.tanggal_permintaan, 'FRS' AS jenis_pemeriksaan,
+        frs.hasil_frs AS hasil, frs.status_frs AS status, frs.penerima_frs AS penerima, frs.tanggal_penerima
+    FROM patient p
+    INNER JOIN frs ON frs.id_pasien = p.id_pasien 
+    WHERE frs.tanggal_permintaan BETWEEN CURDATE() - INTERVAL 14 DAY AND CURDATE()
+
+    UNION ALL
+
+    SELECT p.id_pasien, 
+        p.norm_pasien, 
+        p.nama_pasien, 
+        p.alamat_pasien, 
+        p.tanggal_lahir_pasien, 
+        p.jenis_kelamin_pasien, 
+        srs.unit_asal, srs.dokter_pengirim, srs.diagnosa_klinik, 
+        srs.kode_srs AS kode_pemeriksaan, srs.tanggal_permintaan, 'SRS' AS jenis_pemeriksaan,
+        srs.hasil_srs AS hasil, srs.status_srs AS status, srs.penerima_srs AS penerima, srs.tanggal_penerima
+    FROM patient p
+    INNER JOIN srs ON srs.id_pasien = p.id_pasien 
+    WHERE srs.tanggal_permintaan BETWEEN CURDATE() - INTERVAL 14 DAY AND CURDATE()
+
+    UNION ALL
+
+    SELECT p.id_pasien, 
+        p.norm_pasien, 
+        p.nama_pasien, 
+        p.alamat_pasien, 
+        p.tanggal_lahir_pasien, 
+        p.jenis_kelamin_pasien, 
+        ihc.unit_asal, ihc.dokter_pengirim, ihc.diagnosa_klinik, 
+        ihc.kode_ihc AS kode_pemeriksaan, ihc.tanggal_permintaan, 'IHC' AS jenis_pemeriksaan,
+        ihc.hasil_ihc AS hasil, ihc.status_ihc AS status, ihc.penerima_ihc AS penerima, ihc.tanggal_penerima
+    FROM patient p
+    INNER JOIN ihc ON ihc.id_pasien = p.id_pasien 
+    WHERE ihc.tanggal_permintaan BETWEEN CURDATE() - INTERVAL 14 DAY AND CURDATE()
+
+    ORDER BY tanggal_permintaan DESC, kode_pemeriksaan ASC;
         ";
         $builder = $this->db->query($query);
         return $builder->getResultArray();
