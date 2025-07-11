76e65c7705fd36c99343e56e6e53db62b40e3e3b
BerlianDutaKrisna <berliandutakrisna@gmail.com>
2025-07-11 08:57:45 +0700

    hasil umum

--- app/Views/Exam/index_pencarian.php
+++ app/Views/Exam/index_pencarian.php
@@ -90,6 +90,9 @@
                         <th>Unit Asal</th>
                         <th>Diagnosa Klinik</th>
                         <th>Hasil</th>
+                        <th>Status</th>
+                        <th>Penerima</th>
+                        <th>Tanggal Penerima</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -192,6 +195,11 @@
                                 <td><?= esc($row['unit_asal'] ?? 'Belum Diisi') ?></td>
                                 <td><?= esc($row['diagnosa_klinik'] ?? 'Belum Diisi') ?></td>
                                 <td><?= esc(strip_tags($row['hasil'] ?? 'Belum Ada Hasil')) ?></td>
+                                <td><strong class="<?= in_array($row['status'] ?? '', ['Authorized', 'Selesai']) ? 'text-success' : '' ?>"><?= esc($row['status'] ?? 'Belum Diisi') ?></strong></td>
+                                <td><?= esc($row['penerima'] ?? 'Belum Diterima') ?></td>
+                                <td>
+                                    <?= empty($row['tanggal_penerima']) ? '-' : esc(date('H:i , d-m-Y', strtotime($row['tanggal_penerima']))); ?>
+                                </td>
                             </tr>
                             <?php $i++; ?> <!-- Nomor urut bertambah dalam satu tanggal -->
                         <?php endforeach; ?>
